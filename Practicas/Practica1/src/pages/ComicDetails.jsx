5b27c441af12805bfb7b13e5a5751ac2951f1e51
Eduardo Rueda de Santiago <eduardoruedadesantiago@gmail.com>
2024-11-08 10:24:42 +0100

    Character images appear

--- Practicas/Practica1/src/pages/ComicDetails.jsx
+++ Practicas/Practica1/src/pages/ComicDetails.jsx
@@ -2,6 +2,7 @@ import {useLocation} from 'react-router-dom';
 import '../styles/ComicDetails.css';
 import MarvelService from "../services/Marvel";
 import {useEffect, useState} from "react";
+import Navbar from "../components/Navbar";
 
 function ComicDetails() {
 
@@ -14,6 +15,7 @@ function ComicDetails() {
 
         MarvelService.getComicsCharacters(comicDetails.id).then(
             data => {
+                console.log(data);
                 setComicCharacters(data);
                 setComicCharactersUpdated(true);
             });
@@ -21,26 +23,31 @@ function ComicDetails() {
     }, []);
 
     return (
-        <div className={"comic-detailed-card"}>
-            <label className={"comic-details-title"}>{comicDetails.title}</label>
-            <div className={"comic-details-extended"}>
-                <img className={"comic-details-image"}
-                     src={comicDetails.thumbnail.path + "." + comicDetails.thumbnail.extension}
-                     alt={"Image not found"}
-                />
-                <div className={"comic-details-description"}>
-                    <div className={"comic-details-summary"}>
-                        <p>Summary</p>
-                        {comicDetails.description}
-                    </div>
-                    <div className={"comic-details-characters"}>
-                        {comicCharactersUpdated ? comicCharacters.map((characterData, index) => {
-                            return (
-                                <div key={index} className={"comic-details-character"}>
-                                    <label>{characterData.name}</label>
-                                </div>
-                            );
-                        }) : "Getting characters data"}
+        <div>
+            <Navbar/>
+            <div className={"comic-detailed-card"}>
+                <label className={"comic-details-title"}>{comicDetails.title}</label>
+                <div className={"comic-details-extended"}>
+                    <img className={"comic-details-image"}
+                         src={comicDetails.thumbnail.path + "." + comicDetails.thumbnail.extension}
+                         alt={"Image not found"}
+                    />
+                    <div className={"comic-details-description"}>
+                        <div className={"comic-details-summary"}>
+                            <p>Summary</p>
+                            {comicDetails.description}
+                        </div>
+                        <div className={"comic-details-characters"}>
+                            {comicCharactersUpdated ? comicCharacters.map((characterData, index) => {
+                                return (
+                                    <div key={index} className={"comic-details-character"}>
+                                        <img className={"comic-details-character-image"}
+                                             src={`${characterData.thumbnail.path}.${characterData.thumbnail.extension}`}/>
+                                        <label>{characterData.name}</label>
+                                    </div>
+                                );
+                            }) : "Getting characters data"}
+                        </div>
                     </div>
                 </div>
             </div>
