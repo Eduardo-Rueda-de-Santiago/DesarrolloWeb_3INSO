4e3cc123f7e10d7b7854de91c088b5e71e2346bc
Eduardo Rueda de Santiago <eduardoruedadesantiago@gmail.com>
2024-11-28 09:55:21 +0100

    Onboarding done

--- Practicas/Practica2/src/app/page.tsx
+++ Practicas/Practica2/src/app/page.tsx
@@ -1,8 +1,28 @@
-import Image from "next/image";
+"use client"
+
+import './homeScreen.css'
+import Link from 'next/link';
 
 export default function Home() {
+
     return (
-        <div>
+        <div className={'home-screen-root'}>
+
+            <h1 className={'home-page-title'}>Zo Sale</h1>
+
+            <h2 className={'home-page-function'}>Acceder</h2>
+
+            <Link
+                href='/login'
+                className={'home-page-button login-button'}
+            >Login
+            </Link>
+
+            <Link
+                href='/register'
+                className={'home-page-button register-button'}
+            >Register
+            </Link>
 
         </div>
     );
--- Practicas/Practica2/src/middleware.ts
+++ Practicas/Practica2/src/middleware.ts
@@ -8,27 +8,27 @@ import UserService from "@/services/User";
 const PUBLIC_URLS: Array<string> = ['/', '/login', '/register'];
 
 export function middleware(request: NextRequest) {
-
-    console.log("Made a request");
-
-    if (!(request.nextUrl.pathname in PUBLIC_URLS)) {
-
-        console.log("To non public url")
-
-        const userToken = new UserService().getToken(request.cookies);
-
-        if (!userToken) {
-
-            console.log("Without token")
-
-            const newUrl = new URL('/login', request.url);
-            console.log(newUrl)
-            return NextResponse.redirect(newUrl);
-
-        }
-
-    }
-
+//
+//     console.log("Made a request");
+//
+//     if (!(request.nextUrl.pathname in PUBLIC_URLS)) {
+//
+//         console.log("To non public url")
+//
+//         const userToken = new UserService().getToken(request.cookies);
+//
+//         if (!userToken) {
+//
+//             console.log("Without token")
+//
+//             const newUrl = new URL('/login', request.url);
+//             console.log(newUrl)
+//             return NextResponse.redirect(newUrl);
+//
+//         }
+//
+//     }
+//
 }
 
 export const config = {
