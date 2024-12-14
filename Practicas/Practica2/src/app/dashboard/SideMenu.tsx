445304aa6464a56627b39cf5ed8c4c03044e4375
Eduardo Rueda de Santiago <eduardoruedadesantiago@gmail.com>
2024-12-14 15:30:47 +0100

    Logout implementado

--- Practicas/Practica2/src/app/dashboard/SideMenu.tsx
+++ Practicas/Practica2/src/app/dashboard/SideMenu.tsx
@@ -2,73 +2,89 @@
 
 import Link from "next/link";
 import "./SideMenu.css";
+import UserService from "@/services/User";
+import {useRouter} from "next/navigation";
 
 export default function SideMenu() {
+
+    const router = useRouter();
+
+
+    const logout = () => {
+        new UserService().logout().then(() => {
+            router.push("/");
+        });
+    }
     return (
         <div className={"Side-menu"}>
-            <div>
-                <h3>Clientes</h3>
-                <ul>
-                    <li>
-                        <Link
-                            href='/dashboard/client/explore'
-                            className={''}
-                        >
-                            Explorar
-                        </Link>
-                    </li>
-                    <li>
-                        <Link
-                            href='/dashboard/client/create'
-                            className={''}
-                        >
-                            Crear
-                        </Link>
-                    </li>
-                </ul>
+            <div className={"side-menu-explorer"}>
+                <div>
+                    <h3>Clientes</h3>
+                    <ul>
+                        <li>
+                            <Link
+                                href='/dashboard/client/explore'
+                                className={''}
+                            >
+                                Explorar
+                            </Link>
+                        </li>
+                        <li>
+                            <Link
+                                href='/dashboard/client/create'
+                                className={''}
+                            >
+                                Crear
+                            </Link>
+                        </li>
+                    </ul>
+                </div>
+                <div>
+                    <h3>Proyectos</h3>
+                    <ul>
+                        <li>
+                            <Link
+                                href='/dashboard/project/explore'
+                                className={''}
+                            >
+                                Explorar
+                            </Link>
+                        </li>
+                        <li>
+                            <Link
+                                href='/dashboard/project/create'
+                                className={''}
+                            >
+                                Crear
+                            </Link>
+                        </li>
+                    </ul>
+                </div>
+                <div>
+                    <h3>Albaranes</h3>
+                    <ul>
+                        <li>
+                            <Link
+                                href='/dashboard/deliveryNote/explore'
+                                className={''}
+                            >
+                                Explorar
+                            </Link>
+                        </li>
+                        <li>
+                            <Link
+                                href='/dashboard/deliveryNote/create'
+                                className={''}
+                            >
+                                Crear
+                            </Link>
+                        </li>
+                    </ul>
+                </div>
             </div>
-            <div>
-                <h3>Proyectos</h3>
-                <ul>
-                    <li>
-                        <Link
-                            href='/dashboard/project/explore'
-                            className={''}
-                        >
-                            Explorar
-                        </Link>
-                    </li>
-                    <li>
-                        <Link
-                            href='/dashboard/project/create'
-                            className={''}
-                        >
-                            Crear
-                        </Link>
-                    </li>
-                </ul>
-            </div>
-            <div>
-                <h3>Albaranes</h3>
-                <ul>
-                    <li>
-                        <Link
-                            href='/dashboard/deliveryNote/explore'
-                            className={''}
-                        >
-                            Explorar
-                        </Link>
-                    </li>
-                    <li>
-                        <Link
-                            href='/dashboard/deliveryNote/create'
-                            className={''}
-                        >
-                            Crear
-                        </Link>
-                    </li>
-                </ul>
-            </div>
+
+            <button onClick={logout} className={'logout-button'}>Logout</button>
+
         </div>
     );
 }
